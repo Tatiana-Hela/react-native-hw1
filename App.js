1ac90d39adb8062492ab81dff2a6efaf9999293d
Tatiana-Hela <malya9472@gmail.com>
2023-02-18 01:17:10 +0200

    task 2

--- App.js
+++ App.js
@@ -1,12 +1,111 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
 
 export default function App() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("./assets/images/photo-bg2x.jpg")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <View style={styles.wrapperForm}>
+              <View
+                style={{
+                  ...styles.form,
+                  paddingBottom: setIsShowKeyboard ? 20 : 45,
+                }}
+              >
+                <View>
+                  <Text style={styles.title}>Регистрация</Text>
+                </View>
+                <View>
+                  <TextInput
+                    onFocus={() => {
+                      setIsShowKeyboard(true);
+                    }}
+                    placeholder="Логин"
+                    value={state.login}
+                    onChangeText={(value) =>
+                      setState((prevState) => ({ ...prevState, login: value }))
+                    }
+                    style={styles.input}
+                  />
+                </View>
+                <View>
+                  <TextInput
+                    onFocus={() => {
+                      setIsShowKeyboard(true);
+                    }}
+                    placeholder="Адрес электронной почты"
+                    value={state.email}
+                    onChangeText={(value) =>
+                      setState((prevState) => ({ ...prevState, email: value }))
+                    }
+                    style={styles.input}
+                  />
+                </View>
+                <View>
+                  <TextInput
+                    onFocus={() => {
+                      setIsShowKeyboard(true);
+                    }}
+                    placeholder="Пароль"
+                    value={state.password}
+                    onChangeText={(value) =>
+                      setState((prevState) => ({
+                        ...prevState,
+                        password: value,
+                      }))
+                    }
+                    secureTextEntry={true}
+                    style={styles.input}
+                  />
+                </View>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  onPress={keyboardHide}
+                  style={styles.button}
+                >
+                  <Text style={styles.textButton}>Зарегистрироваться</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -14,7 +113,53 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    // alignItems: "center",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 30,
+    lineHeight: 35,
+    letterSpacing: 0.01,
+    color: "#212121",
+    marginBottom: 27,
+  },
+  input: {
+    fontSize: 16,
+    lineHeight: 19,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    borderRadius: 8,
+    padding: 16,
+    marginTop: 16,
+    color: "#212121",
+  },
+  wrapperForm: {
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  form: {
+    paddingBottom: 45,
+    marginHorizontal: 16,
+  },
+  button: {
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    height: 51,
+    marginTop: 43,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  textButton: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    lineHeight: 19,
   },
 });
